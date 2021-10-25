c2d9f49db3a653b97d8b6943fa9fc9a5d479ce0c
kelong1 <49603313+kelong1@users.noreply.github.com>
2021-10-25 13:57:41 +0300

    Updates

---
+++ precache-manifest.d9b3f29315eb0fecef29b4763d37ab9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45591384df36455f96074a825a8ae6cf",
+    "url": "/react_tours/index.html"
+  },
+  {
+    "revision": "0b8e24ad586aadc00d21",
+    "url": "/react_tours/static/css/main.8e49c9d7.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/react_tours/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_tours/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b8e24ad586aadc00d21",
+    "url": "/react_tours/static/js/main.416e26bb.chunk.js"
+  },
+  {
+    "revision": "a0252ad6742273d141e0",
+    "url": "/react_tours/static/js/runtime-main.8c87278d.js"
+  }
+]);
